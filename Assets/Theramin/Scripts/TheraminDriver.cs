3962b4f18d0b77767cfcc1fba53fd5d28128887c
Joseph Horowitz <contact@josephbhorowitz.com>
2023-07-08 00:14:59 -0400

    Now drive particles based on spherical coordinates

--- Assets/Theramin/Scripts/TheraminDriver.cs
+++ Assets/Theramin/Scripts/TheraminDriver.cs
@@ -6,6 +6,7 @@ using UnityEngine.VFX;
 public class TheraminDriver : MonoBehaviour
 {
     public Transform driver1;
+    public Transform driver2;
     public VisualEffect FX;
 
     public float rotationRate;
@@ -17,11 +18,18 @@ public class TheraminDriver : MonoBehaviour
     {
         float rot = FX.transform.eulerAngles.y;
         rot += rotationRate;
-        FX.transform.eulerAngles = new Vector3(0,rot,90);
-        FX.SetFloat("Driver 0",Vector3.Angle(transform.up,driver1.localPosition)/180);
-        FX.SetFloat("Driver 1",Vector3.Angle(transform.forward,driver1.localPosition)/180);
-        FX.SetFloat("Driver 2",Vector3.Angle(transform.right,driver1.localPosition)/180);
-        FX.SetFloat("Driver 3",Vector3.Angle(transform.up,driver1.up)/180);
-        FX.SetFloat("Driver 4",Vector3.Angle(transform.forward,driver1.forward)/180);
+        // FX.transform.eulerAngles = new Vector3(0,rot,90);
+        Vector3 pos = driver1.localPosition;
+
+        FX.SetFloat("Driver 0",Vector3.Angle(transform.up,pos)/180f);
+        float azimuth = Mathf.Atan(Mathf.Sqrt((Mathf.Pow(pos.x,2) + Mathf.Pow(pos.y,2)))/pos.z)*Mathf.Rad2Deg;
+        if(azimuth < 0) azimuth += 180;
+        FX.SetFloat("Driver 1",azimuth/180);
+
+        pos = driver2.localPosition;
+        FX.SetFloat("Driver 2",Vector3.Angle(transform.up,pos)/180f);
+        azimuth = Mathf.Atan(Mathf.Sqrt((Mathf.Pow(pos.x,2) + Mathf.Pow(pos.y,2)))/pos.z)*Mathf.Rad2Deg;
+        if(azimuth < 0) azimuth += 180;
+        FX.SetFloat("Driver 3",azimuth/180);
     }
 }
