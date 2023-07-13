70dbf164ba6fddbff04b629b98aee4594adc894d
Joseph Horowitz <contact@josephbhorowitz.com>
2023-07-13 11:06:11 -0400

    Finished Color pallet manipulation compute shader for image processing

--- Assets/PhotoEditing/Scripts/ColorPalletExtractor.cs
+++ Assets/PhotoEditing/Scripts/ColorPalletExtractor.cs
@@ -7,9 +7,16 @@ public class ColorPalletExtractor : MonoBehaviour
 {
     private int MAX_DEPTH = 5;
     public Texture2D img;
+    public ComputeBuffer colorBuffer;
+
+    public ComputeBuffer newColorBuffer;
+
+    public RenderTexture outputImage;
 
     public ComputeShader closestColorIndexShader;
-    private RenderTexture closestColorBuffer;
+    public ComputeShader imageRecreator;
+
+    private RenderTexture inputImage;
     public GameObject imagePlane;
     private Material _imgMat;
     private Material imgMat{
@@ -25,29 +32,57 @@ public class ColorPalletExtractor : MonoBehaviour
         Vector3[] photo = getColorArray(img,100,100); //convert photo to array of colors
         colors = getColorPallete(photo,0); //get the colors from a photo
 
-        FindClosestColors();
-        imgMat.SetTexture("_img",closestColorBuffer);
+        InitializeEditor();
+        imgMat.SetTexture("_img",outputImage);
     }
 
-    private void FindClosestColors(){
+    private void Update() {
+        updateColors();
+    }
+
+    private void updateColors(){
+        newColorBuffer.SetData(colors);
+        closestColorIndexShader.SetBuffer(0,"NewColors",newColorBuffer);
+
+        closestColorIndexShader.Dispatch(0,inputImage.width,inputImage.height,1);
+    }
+
+    private void InitializeEditor(){
         //initialize a render texture that is a copy of our image
-        closestColorBuffer = new RenderTexture(img.width,img.height,24);
-        closestColorBuffer.enableRandomWrite = true;
-        closestColorBuffer.Create();
-        RenderTexture.active=closestColorBuffer;
-        Graphics.Blit(img,closestColorBuffer);
+        inputImage = new RenderTexture(img.width,img.height,24);
+        inputImage.enableRandomWrite = true;
+        inputImage.Create();
+        RenderTexture.active=inputImage;
+        Graphics.Blit(img,inputImage);
+
+        //initialize output buffer
+        outputImage = new RenderTexture(img.width,img.height,24);
+        outputImage.enableRandomWrite = true;
+        outputImage.Create();
 
         //pass the color pallete into the shader
-        ComputeBuffer colorBuffer = new ComputeBuffer(colors.Length,sizeof(float)*3);
+        colorBuffer = new ComputeBuffer(colors.Length,sizeof(float)*3);
         colorBuffer.SetData(colors);
-        closestColorIndexShader.SetBuffer(0,"colors",colorBuffer);
+        closestColorIndexShader.SetBuffer(0,"Colors",colorBuffer);
+
+        //pass the color pallete into the shader
+        newColorBuffer = new ComputeBuffer(colors.Length,sizeof(float)*3);
+        newColorBuffer.SetData(colors);
+        closestColorIndexShader.SetBuffer(0,"NewColors",newColorBuffer);
+
         closestColorIndexShader.SetFloat("PalleteSize",colors.Length);
 
         //pass the image into the shader
-        closestColorIndexShader.SetTexture(0,"Result",closestColorBuffer);
-        closestColorIndexShader.SetFloat("ResolutionX",closestColorBuffer.width);
-        closestColorIndexShader.SetFloat("ResolutionY",closestColorBuffer.height);
-        closestColorIndexShader.Dispatch(0,closestColorBuffer.width,closestColorBuffer.height,1);
+        closestColorIndexShader.SetTexture(0,"Input",inputImage);
+        closestColorIndexShader.SetFloat("ResolutionX",inputImage.width);
+        closestColorIndexShader.SetFloat("ResolutionY",inputImage.height);
+
+        //stage output buffer
+        closestColorIndexShader.SetTexture(0,"Output",outputImage);
+
+
+
+        closestColorIndexShader.Dispatch(0,inputImage.width,inputImage.height,1);
     }
 
     private void mergeColor(Vector3[] array, int left, int mid, int right,int ValueToSortBy){
